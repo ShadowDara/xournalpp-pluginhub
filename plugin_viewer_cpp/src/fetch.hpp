c50bb8f6b897b743a2fec32c00ee122083db0d46
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-07-15 20:24:23 +0200

    Update UI Viewer
    Application fetches the `plugins.json` file on button click

---
+++ plugin_viewer_cpp/src/fetch.hpp
@@ -0,0 +1,10 @@
+// fetch.hpp
+#ifndef FETCH_HPP
+#define FETCH_HPP
+
+#include <string>
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
+std::string fetch_url(const std::string& url);
+
+#endif
--- plugin_viewer_cpp/src/main.cpp
+++ plugin_viewer_cpp/src/main.cpp
@@ -1,9 +1,35 @@
+#include <iostream>
 #include <gtk/gtk.h>
+#include "fetch.hpp"
+
+static void on_button_clicked(GtkButton *button, gpointer user_data) {
+    g_print("Button wurde geklickt!\n");
+
+    // Beispiel: Inhalt von example.com fetchen
+    std::string html = fetch_url("https://raw.githubusercontent.com/ShadowDara/xournalpp-plugin-hub-idea/refs/heads/main/plugins.json");
+    std::cout << "Website-Inhalt:\n" << html << std::endl;
+}
 
 static void on_activate(GtkApplication* app, gpointer user_data) {
     GtkWidget *window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Xournal++ Plugin Manager");
     gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
+
+    // Layout-Box erstellen (vertikal)
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_container_set_border_width(GTK_CONTAINER(box), 10);
+    gtk_container_add(GTK_CONTAINER(window), box);
+
+    // Label hinzufügen
+    GtkWidget *label = gtk_label_new("Willkommen zum Plugin-Manager!");
+    gtk_box_pack_start(GTK_BOX(box), label, FALSE, FALSE, 0);
+
+    // Button hinzufügen
+    GtkWidget *button = gtk_button_new_with_label("Plugin hinzufügen");
+    g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+    gtk_box_pack_start(GTK_BOX(box), button, FALSE, FALSE, 0);
+
+    // Start the Application
     gtk_widget_show_all(window);
 }
 
