c7efad8c6b849760fc0fbf28753eab2f534421bf
weuritz8u <203169689+weuritz8u@users.noreply.github.com>
2025-07-16 16:51:08 +0000

    add Astro Webpage

---
+++ webpage/pull_data.js
@@ -0,0 +1,6 @@
+console.log("Pulling data...");
+import { writeFileSync } from 'fs';
+
+// plugins.js erstellen
+//
+// .nojekyll datei erstellen
---
+++ webpage/src/components/in_plugins.ts
@@ -0,0 +1,5 @@
+import { plugin_data } from "./plugins.js"
+
+export async function getPlugins() {
+    return plugin_data;
+}
