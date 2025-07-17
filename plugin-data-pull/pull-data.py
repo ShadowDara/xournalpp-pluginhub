70e99c64d1c2ee47cc83c789972d695fa409cee9
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-07-17 16:44:09 +0200

    update scripts

--- plugin-data-pull/pull-data.py
+++ plugin-data-pull/pull-data.py
@@ -4,6 +4,7 @@ import json
 from pathlib import Path
 from dataclasses import dataclass
 from typing import List, Optional
+import requests
 
 
 # *************************************************
@@ -67,8 +68,32 @@ json_path = os.path.join(base_path, plugindatafolder)
 
 all_files = [os.path.join(json_path, f) for f in os.listdir(json_path)]
 
+# List to store the all registration links in JSON format
 regi_files = []
 
+# List to store the plugin data in JSON format
+plugin_data_list = []
+
+
+# *************************************************
+# *                                               *
+# *                   Functions                   *
+# *                                               *
+# *************************************************
+
+def from_dict(data: dict) -> IndexFile:
+    other_versions_data = data.get("other_versions")
+    other_versions = [OtherVersion(**v) for v in other_versions_data] if other_versions_data else None
+    return IndexFile(
+        name=data["name"],
+        namespace=data["namespace"],
+        author=data["author"],
+        description=data["description"],
+        license=data["license"],
+        version=data["version"],
+        available_for=data["available_for"],
+        other_versions=other_versions
+    )
 
 # *************************************************
 # *                                               *
@@ -76,8 +101,10 @@ regi_files = []
 # *                                               *
 # *************************************************
 
+print("Starting to process plugin registration files...\n")
+
 """
-The Code will go through the file list in pluigin registration,
+The Code will go through the file list in plugin registration,
 check if the json syntax is valid.
 If so, the code will copy the main link and add it to a list of
 Links
@@ -100,16 +127,67 @@ and copy the index file on Github, then look for their
 sub files and althouth copy them!
 """
 for entry in regi_files:
-    print(entry.main_link)
+    # Problem: The Links need to be formatted before
 
+    """
+    Future Problem:
+    an exeption is needed here, if the does not lead to github
+    """
+    #print(entry.main_link)
+    formatted_link = entry.main_link.replace("github.com", "raw.githubusercontent.com").replace("tree/", "refs/heads/")
+    #print(formatted_link)
+
+    response = requests.get(formatted_link + "/index.json")
+
+    if response.status_code == 200:
+        print("Plugin is available, processing...")
+    
+    # Error 1
+    else:
+        # Plugin is not available or the link is broken
+        # so the programm will skip this plugin
+        print(f"Skipping {entry.main_link}")
+        plugin_errors.append(f"Plugin {entry.main_link} could not be accessed. / (1: Status code: {response.status_code})")
+        continue
+
+    try:
+        json_data = json.loads(response.text)
+        index_file = from_dict(json_data)
+        
+        # Now the json syntax of the index file is valid
+
+        # Things which still needs to be checked:
+        # - namespace must be unique
+        # - character check for the namespace
+        # - Most of the values are not allowed to be empty
+        # - the Repository must although contain some code files for the plugin
+
+    # Error 2
+    except Exception as e:
+        print(f"Skipping {entry.main_link}")
+        plugin_errors.append(f"Plugin {entry.main_link} could not be accessed. / (2: Error code: {e})")
+        continue
+
+    # then append the data to the main json file
+    plugin_data_list.append(json_data)
+
+# Save the plugin data to a JSON file
+with open(os.path.join(base_path, "plugins.json"), "w", encoding="UTF-8") as f:
+    json.dump(plugin_data_list, f, ensure_ascii=False, indent=4)
 
 # to end the program
 # check for skipped plugins before exiting
 if plugin_errors != []:
+    print("\n******************************************************************\n")
     print("Some plugins were skipped:")
     for error in plugin_errors:
         print(f" - {error}")
+
+    print("\n******************************************************************\n")
+    print("Exit 1: Some plugins were skipped, please check the errors above.")
     sys.exit(1)
 
 # If everything went fine, exit with code 0
+print("\n******************************************************************\n")
+print("Exit 0: All plugins were processed successfully.")
 sys.exit(0)
